456521b5c2a2a74320201796d352069125f14bd9
zhangchunsheng <zhangchunsheng423@gmail.com>
2020-04-21 11:00:15 +0800

    add place api

--- amap-java-place/src/main/java/com/github/zhangchunsheng/amapplace/bean/result/PoiSearchResult.java
+++ amap-java-place/src/main/java/com/github/zhangchunsheng/amapplace/bean/result/PoiSearchResult.java
@@ -6,13 +6,15 @@ import lombok.EqualsAndHashCode;
 import me.zhangchunsheng.amap.common.bean.result.BaseAmapResult;
 import me.zhangchunsheng.amap.common.util.json.AmapGsonBuilder;
 
+import java.util.List;
+
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class PoiSearchResult extends BaseAmapResult {
     private static final long serialVersionUID = 4587181819499286670L;
 
     @SerializedName("pois")
-    private Poi pois;
+    private List<Poi> pois;
 
     public static PoiSearchResult fromJson(String json) {
         return AmapGsonBuilder.create().fromJson(json, PoiSearchResult.class);
