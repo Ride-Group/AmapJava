b2c4ece17aa097719a50dfec68246851a51b86fe
zhangchunsheng <zhangchunsheng423@gmail.com>
2020-04-25 23:28:05 +0800

    add place api

--- amap-java-place/src/main/java/com/github/zhangchunsheng/amapplace/service/impl/PlaceServiceImpl.java
+++ amap-java-place/src/main/java/com/github/zhangchunsheng/amapplace/service/impl/PlaceServiceImpl.java
@@ -7,6 +7,9 @@ import me.zhangchunsheng.amap.common.constant.AmapConstants;
 import me.zhangchunsheng.amap.common.exception.AmapException;
 import me.zhangchunsheng.amap.common.service.impl.AmapServiceApacheHttpImpl;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 /**
  * <pre>
  * 高德地图接口请求实现类，默认使用Apache HttpClient实现
@@ -18,12 +21,25 @@ import me.zhangchunsheng.amap.common.service.impl.AmapServiceApacheHttpImpl;
 public class PlaceServiceImpl extends AmapServiceApacheHttpImpl implements PlaceService {
     @Override
     public InputTips inputTips(String city, String keywords) throws AmapException {
+        try {
+            // URL 编码
+            keywords = URLEncoder.encode(keywords, "utf-8");
+        } catch (UnsupportedEncodingException e) {
+            // do nothing
+        }
+
         String responseContent = this.get(String.format(this.getConfig().getBaseUrl() + AmapConstants.Url.INPUTTIPS, city, keywords, this.getConfig().getKey()));
         return InputTips.fromJson(responseContent);
     }
 
     @Override
     public PoiSearchResult placeText(String city, String keywords, String page, String offset) throws AmapException {
+        try {
+            // URL 编码
+            keywords = URLEncoder.encode(keywords, "utf-8");
+        } catch (UnsupportedEncodingException e) {
+            // do nothing
+        }
         String responseContent = this.get(String.format(this.getConfig().getBaseUrl() + AmapConstants.Url.PLACE_TEXT, city, keywords, page, offset, this.getConfig().getKey()));
         return PoiSearchResult.fromJson(responseContent);
     }
