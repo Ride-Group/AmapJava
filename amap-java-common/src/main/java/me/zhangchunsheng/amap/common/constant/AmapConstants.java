55ba1131fbf8e394388c0a39471355ac0c72d437
zhangchunsheng <zhangchunsheng423@gmail.com>
2020-04-27 10:35:17 +0800

    add distance api

--- amap-java-common/src/main/java/me/zhangchunsheng/amap/common/constant/AmapConstants.java
+++ amap-java-common/src/main/java/me/zhangchunsheng/amap/common/constant/AmapConstants.java
@@ -24,6 +24,20 @@ public class AmapConstants {
     }
 
     /**
+     * 0：直线距离
+     1：驾车导航距离（仅支持国内坐标）。
+     必须指出，当为1时会考虑路况，故在不同时间请求返回结果可能不同。
+     此策略和驾车路径规划接口的 strategy=4策略基本一致，策略为“ 躲避拥堵的路线，但是可能会存在绕路的情况，耗时可能较长 ”
+     若需要实现高德地图客户端效果，可以考虑使用驾车路径规划接口
+     3：步行规划距离（仅支持5km之间的距离）
+     */
+    public static class DistanceType {
+        public static final int LINE = 0;
+        public static final int DRIVE = 1;
+        public static final int WALK = 2;
+    }
+
+    /**
      * 结果代码.
      */
     public static class ResultStatus {
